526404818557bc7cd69747224e1cc98337d46aad
Joshua Tony <64730665+joshuatonyy@users.noreply.github.com>
2024-12-08 23:56:55 +0700

    Added logo to the homepage header

--- reddit-clone-frontend/src/pages/HomePage/HomePage.jsx
+++ reddit-clone-frontend/src/pages/HomePage/HomePage.jsx
@@ -1,29 +1,36 @@
 import React, { useState } from "react";
-import './HomePage.css'
+import "./HomePage.css";
 import { AuthForm } from "../../components/AuthForm/AuthForm";
+import logo from "../../assets/tendit_logo.png"
 
 export const HomePage = () => {
   const [isAuthFormVisible, setIsAuthFormVisible] = useState(false);
 
-    const handleLogin = () => {
-        setIsAuthFormVisible(true);
-    }
+  const handleLogin = () => {
+    setIsAuthFormVisible(true);
+  };
 
-    const handleCloseForm = () => {
-      setIsAuthFormVisible(false);
-    }
+  const handleCloseForm = () => {
+    setIsAuthFormVisible(false);
+  };
 
   return (
     <div className="homepage__container">
       <div className="homepage__header">
-        <p className="homepage__header-title">Tendit</p>
-          <p className="homepage__header-login-logout" onClick={handleLogin}>
-            Login
-          </p>
-       
+        <div className="homepage__header-left">
+          <img
+            src={logo}
+            alt="Tendit Logo"
+            className="homepage__header-logo"
+          />
+          <p className="homepage__header-title">Tendit</p>
+        </div>
+        <p className="homepage__header-login-logout" onClick={handleLogin}>
+          Login
+        </p>
       </div>
 
-      {isAuthFormVisible && <AuthForm onClose={handleCloseForm}/>}
+      {isAuthFormVisible && <AuthForm onClose={handleCloseForm} />}
     </div>
   );
 };
